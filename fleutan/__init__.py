976770fde12a3ef367ad55d2d134f01c5cd44508
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2017-07-16 20:57:43 +0200

    fleutan: python packaging
    for pip
    
    Signed-off-by: Matthias Tafelmeier <matthias.tafelmeier@gmx.net>

---
+++ fleutan/__init__.py
@@ -0,0 +1,1 @@
+from .fleutan import run
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name='Fleutan',
+  packages=['fleutan', 'fleutan.utils'],
+  license="GPL-3.0",
+  version='0.6',
+  description="Fleutan - a scalable network flows and paths wielding lever",
+  author='Matthias Tafelmeier',
+  author_email='matthias.tafelmeier@gmx.net',
+  scripts=['./fleutan/fleutan'],
+  url='https://github.com/cherusk/fleutan',
+  download_url='https://github.com/cherusk/fleutan/dist/0.6.tar.gz',
+  install_requires=['tabulate', 'futures', 'ascii_graph', 'numpy', 'psutil', 'multiprocessing', 'backports.functools_lru_cache', 'termcolor' ],
+  keywords=['linux', 'tool', 'transport', 'sockets', 'flow', 'stats', 'analytics'],
+  classifiers=[],
+)
